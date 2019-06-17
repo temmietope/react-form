81515f9b36ae089559c7dabb2bc70693284abfbb
Temitope <temmieayodele@gmail.com>
2019-06-17 23:29:58 +0100

    new edits

---
+++ src/Component/Userslist.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Userslist extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Userslist
