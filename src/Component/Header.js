6c72ca9b2577ebac1cc80cbe0b9e2817aa9cee66
Temitope <temmieayodele@gmail.com>
2019-06-17 23:29:06 +0100

    new edits

---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="Header" style={{textAlign: 'center'}}>
+                <h1>Form</h1>
+            </div>
+        )
+    }
+};
+
+export default Header;
