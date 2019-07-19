f60a93683bf38513561ca6c0ab63d42a0dff9656
Temitope <temmieayodele@gmail.com>
2019-07-19 21:27:26 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./Component/Header.js";
-import Form from "./Component/Form.js";
-import Userslist from "./Component/Userslist.js"
+import Home from "./Home";
+import UsersList from "./Component/Userslist";
 
-class App extends Component {
-  state = {
-    users: [],
-  };
-  addUser= (user)=>{
-    this.setState({ users: [...this.state.users, user] },()=>{
-      console.log(this.state.users)
-
-    });
-  }
-  render() {
-    return (
+function App() {
+  return (
+    <div className="App">
       <Router>
-        <div className="App">
-          <div className="wrapper">
-            <Header />
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <React.Fragment>
-                  <Form addUser={this.addUser}/>
-                </React.Fragment>
-              )}
-            />
-            <Route
-              path="/users-list"
-              render={props => (
-                <React.Fragment>
-                  <Userslist userslist={this.state.users}/>
-                </React.Fragment>
-              )}
-            />
-          </div>
-        </div>
-    </Router>
-    );
-  }
+        <>
+          <Route exact path="/" component={Home} />
+          <Route path="/users-list" component={UsersList} />
+        </>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
