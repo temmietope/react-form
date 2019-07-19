acb833020493e7133adf9a52b0eff93f6cb3e45d
Temitope <temmieayodele@gmail.com>
2019-07-19 21:25:23 +0100

    changed to function component

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,34 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { rand } from "../utils/random";
+import Userslist from "./Userslist";
 
-class Form extends Component {
-  state = {
-    uniqueID: "",
+
+const Form = () => {
+  const [register, setRegister] = useState({
     firstname: "",
     lastname: "",
     address: "",
     birthDate: "",
     message: ""
-  };
-  onChange = e => {
-    this.setState({
+  });
+  const[users, setUsers] = useState([])
+
+  const onChange = e => {
+    setRegister({
+      ...register,
       [e.target.name]: e.target.value
     });
   };
-  onSubmit = e => {
+  const onSubmit = e => {
+    const { firstname, lastname, address, birthDate, message } = register;
     e.preventDefault();
-    const { firstname, lastname, address, birthDate, message } = this.state;
-    if (
-      !this.refs.firstname.value ||
-      !this.refs.lastname.value ||
-      !this.refs.address.value ||
-      !this.refs.birthdate.value ||
-      !this.refs.message.value
-    ) {
-      return alert("Fill all the required boxes");
-    }
-
+    console.log(register);
     const user = {
       firstname,
       lastname,
@@ -37,102 +32,107 @@ class Form extends Component {
       message,
       uniqueID: rand()
     };
-    this.props.addUser(user);
-
-    console.log(user);
-    this.refs.firstname.value = "";
-    this.refs.lastname.value = "";
-    this.refs.address.value = "";
-    this.refs.birthdate.value = "";
-    this.refs.message.value = "";
+    setUsers(users.concat(user))
   };
-  render() {
-    return (
-      <form onSubmit={this.onSubmit}>
-        <div
-          className="form"
-          style={{
-            display: "flex",
-            flexFlow: "column",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <div className="signup" style={{ textAlign: "center" }}>
-            <h2>Sign up for free</h2>
-          </div>
-
-          <div className="userName">
-            <h4>
-              Name: <span style={{ color: "red" }}>*</span>
-            </h4>
-            <input
-              type="text"
-              name="firstname"
-              placeholder="Firstname"
-              ref="firstname"
-              onChange={this.onChange}
-            />
-            <input
-              type="text"
-              name="lastname"
-              placeholder="Lastname"
-              ref="lastname"
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="userAddress">
-            <h4>
-              Address: <span style={{ color: "red" }}>*</span>
-            </h4>
-            <input
-              type="text"
-              name="address"
-              placeholder="Your address"
-              ref="address"
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="userBirthDate">
-            <h4>
-              Date of Birth: <span style={{ color: "red" }}>*</span>
-            </h4>
-            <input
-              type="text"
-              name="birthDate"
-              placeholder="dd/mm"
-              ref="birthdate"
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="message">
-            <h4>
-              Favorite Quote: <span style={{ color: "red" }}>*</span>
-            </h4>
-            <input
-              type="text"
-              name="message"
-              placeholder="Your favorite quote"
-              ref="message"
-              style={{ height: "50px" }}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="submit">
-            <input type="submit" value="Submit" className="btn" />
-          </div>
-          <div className="check users" style={{ marginTop: "30px" }}>
-            <Link
-              to="/users-list"
-              style={{ color: "green", textDecoration: "none" }}
-            >
-              Click here to check UsersList
-            </Link>
-          </div>
+  return (
+    <>
+    <form
+      onSubmit={e => {
+        onSubmit(e);
+      }}
+    >
+      <div
+        className="form"
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <div className="signup" style={{ textAlign: "center" }}>
+          <h2>Sign up for free</h2>
         </div>
-      </form>
-    );
-  }
-}
+
+        <div className="userName">
+          <h4>
+            Name: <span style={{ color: "red" }}>*</span>
+          </h4>
+          <input
+            type="text"
+            name="firstname"
+            placeholder="Firstname"
+            onChange={e => {
+              onChange(e);
+            }}
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Lastname"
+            onChange={e => {
+              onChange(e);
+            }}
+          />
+        </div>
+        <div className="userAddress">
+          <h4>
+            Address: <span style={{ color: "red" }}>*</span>
+          </h4>
+          <input
+            type="text"
+            name="address"
+            placeholder="Your address"
+            onChange={e => {
+              onChange(e);
+            }}
+          />
+        </div>
+        <div className="userBirthDate">
+          <h4>
+            Date of Birth: <span style={{ color: "red" }}>*</span>
+          </h4>
+          <input
+            type="text"
+            name="birthDate"
+            placeholder="dd/mm"
+            onChange={e => {
+              onChange(e);
+            }}
+          />
+        </div>
+        <div className="message">
+          <h4>
+            Favorite Quote: <span style={{ color: "red" }}>*</span>
+          </h4>
+          <input
+            type="text"
+            name="message"
+            placeholder="Your favorite quote"
+            style={{ height: "50px" }}
+            onChange={e => {
+              onChange(e);
+            }}
+          />
+        </div>
+        <div className="submit">
+          <input type="submit" value="Submit" className="btn" />
+        </div>
+        <div className="check users" style={{ marginTop: "30px" }}>
+          <Link
+            to="/users-list"
+            style={{ color: "green", textDecoration: "none" }}
+          >
+            Click here to check UsersList
+          </Link>
+        </div>
+      </div>
+    </form>
+    <div className="userslist">
+    <Userslist userslist={users}/>
+  </div>
+  </>
+  );
+};
 
 export default Form;
