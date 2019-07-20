69ae3d94bae80c845e4694e0dcb392d4b67801f5
Temitope <temmieayodele@gmail.com>
2019-07-20 17:23:38 +0100

    passed prop from the Form component

--- src/Component/Userslist.js
+++ src/Component/Userslist.js
@@ -1,21 +1,21 @@
-import React, { Fragment, Component } from "react";
+import React, { useEffect } from "react";
 import Users from "./Users";
 import { Link } from "react-router-dom";
 
-class Userslist extends Component {
-  render() {
-    return (
-      <Fragment>
-       <h2>List of Subscribers</h2>
-       <Link to="/">Back to form</Link>
-        {this.props.userslist.map(user => {
-          return <Users key={user.uniqueID} user={user} />;
-        })}
-
-         
-      </Fragment>
-    );
-  }
-}
+const Userslist = props => {
+  useEffect(() => {
+    console.log(props);
+  });
+  const { state } = props.location;
+  return (
+    <>
+      <h2>List of Subscribers</h2>
+      <Link to="/">Back to form</Link>
+      {state.map(user => {
+        return <Users key={user.uniqueID} user={user} />;
+      })}
+    </>
+  );
+};
 
 export default Userslist;
