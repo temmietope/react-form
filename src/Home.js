c42338118f5620b9d21362b748f2128c2f45efb2
Temitope <temmieayodele@gmail.com>
2019-07-20 17:24:36 +0100

    formatted code

--- src/Home.js
+++ src/Home.js
@@ -7,50 +7,10 @@ const Home = () => {
     <div className="App">
       <div className="wrapper">
         <Header />
-        <Form/>
+        <Form />
       </div>
-      
     </div>
   );
 };
-
-// class App extends Component {
-//   state = {
-//     users: []
-//   };
-//   addUser = user => {
-//     this.setState({ users: [...this.state.users, user] }, () => {
-//       console.log(this.state.users);
-//     });
-//   };
-//   render() {
-//     return (
-//       <Router>
-//         <div className="App">
-//           <div className="wrapper">
-//             <Header />
-//             <Route
-//               exact
-//               path="/"
-//               render={props => (
-//                 <React.Fragment>
-//                   <Form addUser={this.addUser} />
-//                 </React.Fragment>
-//               )}
-//             />
-//             <Route
-//               path="/users-list"
-//               render={props => (
-//                 <React.Fragment>
-//                   <Userslist userslist={this.state.users} />
-//                 </React.Fragment>
-//               )}
-//             />
-//           </div>
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
 
 export default Home;
